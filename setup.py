5a75471bfa8ff57deff0000f452681f640ee7120
Jeremiah Campbell <jeremiah.campbell@meantheory.com>
2013-11-16 22:08:24 -0500

    added setup.py file for managing install of module

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tk64',
+      version='0.1',
+      description='Token encoding and decoding for 64 bit integers',
+      author='Jeremiah Campbell',
+      author_email='jeremiah.campbell@gmail.com',
+      url='https://github.com/meantheory/tk64',
+     )
